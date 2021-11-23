9bd4ecda5f4bf349c9fcf46ec63e6bc5759a9424
rootn3rd <krishnahare14@gmail.com>
2021-11-24 02:14:24 +0530

    print method removed

--- index.ts
+++ index.ts
@@ -1,41 +1,34 @@
 import './style.css';
 
-import { of, map, Observable, Observer, interval, take } from 'rxjs';
+import { of, map, Observable, Observer, interval, take, from } from 'rxjs';
 
 import { myOperator, myOperatorWithParameter } from './operators/myoperator';
 import { myCombineLatest } from './operators/mycombineLatest';
 import { myFilter } from './operators/myfilter';
 import { myInterval } from './operators/myinterval';
 
-const print = (groupName: string, execute: () => void) => {
-  console.group(groupName);
-  execute();
-  console.groupEnd();
-};
+of(1)
+  .pipe(myOperator)
+  .subscribe((x) => console.log('myOperator:', x));
 
-print('myOperator', () => of(1).pipe(myOperator).subscribe(console.log));
+of(2)
+  .pipe(myOperatorWithParameter('jamesbond'))
+  .subscribe((x) => console.log('myOperatorWithParameter:', x));
 
-print('myOperatorWithParameter', () =>
-  of(2).pipe(myOperatorWithParameter('jamesbond')).subscribe(console.log)
+let obs1 = from([1, 2]);
+let obs2 = from([3, 4]);
+const sub = myCombineLatest(obs1, obs2).subscribe((x) =>
+  console.log('myCombineLatest', x)
 );
+//sub.unsubscribe()
 
-print('myCombineLatest', () => {
-  let obs1 = interval(20).pipe(take(2));
-  let obs2 = interval(20).pipe(take(2));
-  const sub = myCombineLatest(obs1, obs2).subscribe(console.log);
-  //sub.unsubscribe()
-});
-
-print('myFilter', () => {
-  let obs1 = interval(30).pipe(
+from([1, 2, 3, 4, 5, 6])
+  .pipe(
     myFilter((f) => f % 2 == 0),
     take(4)
-  );
-  obs1.subscribe(console.log);
-});
+  )
+  .subscribe((x) => console.log('myFilter:', x));
 
-print('myInterval', () => {
-  myInterval(200)
-    .pipe(take(10))
-    .subscribe((x) => console.log('myInterval:', x));
-});
+myInterval(200)
+  .pipe(take(4))
+  .subscribe((x) => console.log('myInterval:', x));
